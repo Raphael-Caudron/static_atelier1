b89a5bca73132391f54f9ab30521f357c37076c6
Raphael <raphael0450@gmail.com>
2023-05-02 20:51:59 +0200

    post api c'est bon

--- atelierI/js/addCard.js
+++ atelierI/js/addCard.js
@@ -8,7 +8,7 @@ form.addEventListener('submit', (event) => {
     console.log("DEDANS")
     const formData = new FormData(form);
 
-    const card = {
+    let card = {
         smallImgUrl: formData.get('smallImgUrl'),
         family: formData.get('family'),
         imgUrl: formData.get('imgUrl'),
@@ -17,13 +17,37 @@ form.addEventListener('submit', (event) => {
         hp: formData.get('hp'),
         energy: formData.get('energy'),
         attack: formData.get('attack'),
-        defense: formData.get('defense')
+        defense: formData.get('defense'),
+        userID: 0,
+        id: 0,
+        price: 100,
+        affinity: "shadow"
     };
 
+    //ajout local
     const data = localStorage.getItem("monFichier")
     const parsedData = JSON.parse(data)
     parsedData.push(card)
     localStorage.setItem("monFichier", JSON.stringify(parsedData));
 
     console.log(card);
+
+    //Post Api
+    let urlPost="http://vps.cpe-sn.fr:8083/card"; 
+    let context =   {
+                        method: 'POST',
+                        headers: {
+                            'Accept': '*/*',
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(card)
+                    };
+        
+    fetch(urlPost,context)
+        .then(response => response.json())
+        .catch(error => err_callback(error));
 });
+
+function err_callback(error){
+    console.log(error);
+}
